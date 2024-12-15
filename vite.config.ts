2c6172a8cde89cc66923f2a6b1fc9a6aef2e788c
Belkayou07 <belkayou07@gmail.com>
2024-12-16 00:20:04 +0100

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/project/', // replace 'project' with your repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: process.env.NODE_ENV === 'production' 
-    ? '/' + process.env.npm_package_name + '/' 
-    : '/',
 });
