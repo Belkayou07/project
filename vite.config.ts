c7b7a4f56c1939ef3d775dd0f73993ad14d15944
Belkayou07 <belkayou07@gmail.com>
2024-12-16 00:31:01 +0100

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/project/', // Change this to relative path
+  base: '/project/',  // Change this line
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
